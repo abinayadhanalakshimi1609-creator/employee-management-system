8be46cdbb336e768b450744e976de217618015a4
Abinaya <abinayadhanalakshimi1609@gmail.com>
2026-07-18 15:08:31 +0530

    Added MongoDB Atlas connection

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use("/api", employeeRoutes);
 app.use("/api", authRoutes);
 
-mongoose.connect("mongodb://127.0.0.1:27017/employeeDB")
+mongoose.connect("process.eve.MONGO_URI")
 .then(() => {
     console.log("MongoDB Connected Successfully");
 })
